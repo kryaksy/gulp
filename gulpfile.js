ae75b207e56550ecfc6e0b5689eed5cb4e20bb7c
Koray <ib.koray@github.com>
2017-12-26 04:34:52 +0300

    Preprocessing with Gulp
    - Real Tasks Look Like
    - npm install gulp-sass --save-dev
    - requiring gulp-sass plugin
    - .scss to .css

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
 
-// [$ gulp hi] returns 'Hello Ibo!'
+// [$ gulp hi] returns 'Hello Ibo!':
 gulp.task('hi', function() {
 	// Stuff here
 	console.log('Hi Ibo!');
 });
+
+//SASS
+gulp.task('sass', function () {
+  return gulp.src('app/scss/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+})
