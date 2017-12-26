385f58f9c11603e09633997d494ca45bc3ed8be6
kryaksy <ib.koray@gmail.com>
2017-12-26 08:00:59 +0300

    browserSync
    - var browserSync
    - piping browserSync
    - changing Sass slightly
    - creating browserSync task
    - tested with Opera-Dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function(){
+  browserSync.init({
+    server: {
+      baseDir:'app'
+    },
+  })
+})
 
 gulp.task('sass', function(){
     gulp.src('app/scss/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('app/css'));
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+          stream: true
+        }))
 });
 
 gulp.task('watch', function () {
 	gulp.watch('app/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  //Other Watchers
 });
