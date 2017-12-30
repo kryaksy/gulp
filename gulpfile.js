1ba5eb0a2a2b123e071ae041c7cf78d461176fc0
kryaksy <ib.koray@gmail.com>
2017-12-30 13:26:48 +0300

    Revert "Merge pull request #8 from islam-muhammad/master"
    This reverts commit 022f8b90fe111473f3910da7508e42e472bec519, reversing
    changes made to 12152aa3580946b26b88fdeaca821c8d6cc0426e.

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,27 @@
-// REQUIRES
-const gulp = require('gulp');
-const useref = require('gulp-useref');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
-// TASKS
 
-//useref task
-gulp.task('useref', () => {
-  return gulp.src('app/*.html')
-    .pipe(useref())
-    .pipe(gulp.dest('dist'));
-});
-
-//html task
-
-// Browser Sync task
-gulp.task('browserSync', () => {
+gulp.task('browserSync', function(){
   browserSync.init({
     server: {
-      baseDir: 'app'
+      baseDir:'app'
     },
   })
 })
 
-// Sass task
-gulp.task('sass', () => {
-  return gulp.src('app/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-})
+gulp.task('sass', function(){
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({
+          stream: true
+        }))
+});
 
-// Watch task
-gulp.task('watch', ['browserSync', 'sass'], () => {
-  gulp.watch('app/scss/**/*.scss', ['sass'])
-  gulp.watch('app/js/**/*.js', browserSync.reload)
-  gulp.watch('app/*.html', browserSync.reload)
-})
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/js/**/*.scss', browserSync.reload);
+  gulp.watch('app/*.html', browserSync.reload);
+});
